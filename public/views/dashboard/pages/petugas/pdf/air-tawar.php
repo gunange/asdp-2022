6074f0e5fc376870a67541754602fe145ee02069
gunange <gunangeh7@gmail.com>
2022-10-16 19:02:22 +0900

    Perbaikan JSPDF

--- public/views/dashboard/pages/petugas/pdf/air-tawar.php
+++ public/views/dashboard/pages/petugas/pdf/air-tawar.php
@@ -19,42 +19,48 @@
 
 		<script>
 			var dataPDF = <?= json_encode($dataPdf) ?>
-			// console.log(dataPDF)
+			
 			var doc = new jsPDF({
-				orientation: 'p',
-				unit: 'mm',
-				format: [210, 330]
-			});
+							orientation: 'p',
+							unit: 'mm',
+							format: 'a4' // Ukurran (lebar x tinggi)
+							//format: [500, 900] // Ukurran (lebar x tinggi)
+						});
 
 
 			doc.setFontSize(11).setFont(undefined, 'bold');
-			doc.text(23, 20, "PT. ASDP Indonesia Ferry (Persero)");
-			doc.text(36, 25, "Cabang Ternate");
+			
+			let img = new Image();
+			img.src = "https://project.me/asdp-ternate/public/files/images/logo-asdp.png";
+			
+			doc.addImage(img, 'jpeg', 23, 5, 50, 20); 
+			doc.text(23, 30, "PT. ASDP Indonesia Ferry (Persero)");
+			doc.text(36, 35, "Cabang Ternate");
 			doc.setFontSize(12);
 
-			doc.text(40, 30, "AIR TAWAR").setFont(undefined, 'bold');
+			doc.text(40, 40, "AIR TAWAR").setFont(undefined, 'bold');
 
 
 			doc.setFontSize(10);
-			doc.text(20, 40, "Tanggal").setFont(undefined, 'normal');
-			doc.text(40, 40, ": " + dataPDF['hari'] + " , " + dataPDF['tgl'] + " , " + dataPDF['waktu']).setFont(undefined, 'bold');
-			doc.text(20, 45, "Operator").setFont(undefined, 'normal');
-			doc.text(40, 45, ": " + dataPDF['nama_admin']).setFont(undefined, 'bold');
-			doc.text(20, 50, "Regu").setFont(undefined, 'normal');
-			doc.text(40, 50, ": " + dataPDF['tgl']).setFont(undefined, 'normal');
+			doc.text(20, 50, "Tanggal").setFont(undefined, 'normal');
+			doc.text(40, 50, ": " + dataPDF['hari'] + " , " + dataPDF['tgl'] + " , " + dataPDF['waktu']).setFont(undefined, 'bold');
+			doc.text(20, 55, "Operator").setFont(undefined, 'normal');
+			doc.text(40, 55, ": " + dataPDF['nama_admin']).setFont(undefined, 'bold');
+			doc.text(20, 60, "Regu").setFont(undefined, 'normal');
+			doc.text(40, 60, ": " + dataPDF['tgl']).setFont(undefined, 'normal');
 
 
-			doc.text(20, 65, dataPDF['nama_kapal']).setFont(undefined, 'bold');
-			doc.text(20, 70, "Dermaga").setFont(undefined, 'normal');
-			doc.text(60, 70, ": " + dataPDF['dermaga']).setFont(undefined, 'bold');
-			doc.text(20, 75, "Debit Air").setFont(undefined, 'normal');
-			doc.text(60, 75, ": " + dataPDF['debit_air']).setFont(undefined, 'bold');
-			doc.text(20, 80, "Tarif").setFont(undefined, 'normal');
-			doc.text(60, 80, ": Rp. 50,/Liter").setFont(undefined, 'bold');
+			doc.text(20, 75, dataPDF['nama_kapal']).setFont(undefined, 'bold');
+			doc.text(20, 80, "Dermaga").setFont(undefined, 'normal');
+			doc.text(60, 80, ": " + dataPDF['dermaga']).setFont(undefined, 'bold');
+			doc.text(20, 85, "Debit Air").setFont(undefined, 'normal');
+			doc.text(60, 85, ": " + dataPDF['debit_air']).setFont(undefined, 'bold');
+			doc.text(20, 90, "Tarif").setFont(undefined, 'normal');
+			doc.text(60, 90, ": Rp. 50,/Liter").setFont(undefined, 'bold');
 
 
-			doc.text(20, 90, "Tagihan Air Tawar").setFont(undefined, 'bold');
-			doc.text(60, 90, ": Rp. " + dataPDF['total_air_tawar']).setFont(undefined, 'bold');
+			doc.text(20, 100, "Tagihan Air Tawar").setFont(undefined, 'bold');
+			doc.text(60, 100, ": Rp. " + dataPDF['total_air_tawar']).setFont(undefined, 'bold');
 			doc.text(20, 95, "______________________________________").setFont(undefined, 'bold');
 
 
