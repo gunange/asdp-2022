1f26a753c60a741dd0f0bf27335f587f2bb1e96f
gunange <gunangeh7@gmail.com>
2022-10-17 14:19:54 +0900

    Perbaikan Data Sandar

--- public/views/dashboard/pages/petugas/sandar.php
+++ public/views/dashboard/pages/petugas/sandar.php
@@ -22,7 +22,7 @@
 
         <div class="col-6 col-md-6" id="dermaga<?= $k ?>">
             <div class="card card-small shadow bx-shadow">
-                <form class="" method="POST" action="<?= $this->gLink ?>SetSandar/postForm">
+                <form class="data-form" method="POST" action="<?= $this->gLink ?>SetSandar/postForm">
 
                     <div class="card-header border-bottom bg-white">
                         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center my-3">
@@ -44,13 +44,17 @@
 
                                 <div class="col-md-12 mb-3">
                                     <label class="form-label">Pilih Kapal</label>
-                                    <select name="id_kapal" id="id_kapal<?= $k ?>" required class="form-select form-select-sm sel-all set-default<?= $k ?>">
+                                    <select name="data_id_gt" id="data_id_gt<?= $k ?>" required class="form-select form-select-sm sel-all set-default<?= $k ?>">
                                         <option value=""></option>
                                         <?php foreach ($this->model->GetKapal() as $opsi) : ?>
-                                            <option value="<?= $opsi['id'] ?>"><?= $opsi['nama_kapal'] ?></option>
+                                            <option value="<?= $opsi['id'] . '-' . $opsi['gt'] ?>"><?= $opsi['nama_kapal'] ?></option>
                                         <?php endforeach; ?>
                                     </select>
                                 </div>
+
+                                <input type="hidden" name="gt" id="gt<?= $k ?>">
+                                <input type="hidden" name="id_kapal" id="id_kapal<?= $k ?>">
+                                <input type="hidden" id="tarif_dermaga<?= $k ?>" value="<?= $dermaga['tarif'] ?>">
 
                                 <div class="col-12 mb-3">
                                     <label class="form-label">Start</label>
@@ -150,8 +154,17 @@
     }
 
     function startTime(id = "") {
+
         document.getElementById('dermaga' + id).classList.add('active');
-        setTime(id);
+        var dataKapal = document.getElementById('data_id_gt' + id ).value ;
+        if (dataKapal !== ""){
+            ex = dataKapal.split("-");
+            
+            document.getElementById('id_kapal' + id).value = ex[0];
+            document.getElementById('gt' + id).value = ex[1];
+
+            setTime(id);
+        }
 
     }
 
@@ -160,7 +173,7 @@
         var lamaWaktu = HitungRangeWaktu(nTime);
         var call = HitungCall(nTime);
 
-        HitungTarifSandar()
+        HitungTarifSandar(call, id)
 
         document.getElementById('dermaga' + id).classList.remove('active');
         document.getElementById('tombol-waktu' + id).innerHTML =
@@ -181,8 +194,10 @@
                     </button>`;
         document.getElementById('live' + id).value = "";
         document.getElementById('stop' + id).value = "";
+        document.getElementById('id_kapal' + id).value = "";
+        document.getElementById('total_sandar' + id).value = "" ;
         document.getElementById('start' + id).innerHTML = "";
-        $('#id_kapal' + id).val('').trigger('change');
+        $('#data_id_gt' + id).val('').trigger('change');
         $('#status_debit_air' + id).val('').trigger('change');
 
 
@@ -190,9 +205,13 @@
 </script>
 
 <script>
-    function HitungTarifSandar() {
-        console.log(call);
+    function HitungTarifSandar(call, id) {
+ 
+        var tarifDermaga =  document.getElementById('tarif_dermaga' + id).value
+        var gt = document.getElementById('gt' + id).value ;
+        var tarif = (tarifDermaga * call * gt ) - ((tarifDermaga * call * gt ) * 2 / 100) ;
 
+        document.getElementById('total_sandar' + id).value = tarif;
     }
 
     function HitungRangeWaktu(n) {
