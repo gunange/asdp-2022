dc2d0d6b5b783d20c78737937b085475c62b406f
Rieyani <gunangeh7@gmail.com>
2024-12-15 22:54:12 +0900

    up db def

--- db/default-db.sql
+++ db/default-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 15, 2024 at 10:23 AM
+-- Generation Time: Dec 15, 2024 at 01:53 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.2.13
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `asdp-2022`
+-- Database: `asdp-v2`
 --
 
 -- --------------------------------------------------------
@@ -36,6 +36,7 @@ CREATE TABLE `tbl_air_tawar` (
   `tgl` date NOT NULL,
   `waktu` time NOT NULL,
   `debit_air` double NOT NULL,
+  `tarif_air` double NOT NULL,
   `total_air_tawar` double NOT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -57,8 +58,9 @@ CREATE TABLE `tbl_dermaga` (
 --
 
 INSERT INTO `tbl_dermaga` (`id`, `dermaga`, `tarif`) VALUES
-(1, 'Dermaga 1', 150),
-(2, 'Dermaga 2', 85);
+(1, 'Valve 1', 150),
+(2, 'Valve 2', 85),
+(5, 'Valve 3', 10);
 
 -- --------------------------------------------------------
 
@@ -233,31 +235,7 @@ CREATE TABLE `tbl_kapal` (
 --
 
 INSERT INTO `tbl_kapal` (`id`, `nama_kapal`, `perusahaan`, `gt`, `pajak`, `kamera`) VALUES
-(1, 'KMP. PANORAMA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 7965, '-', ''),
-(74, 'KMP. PELANGI NUSANTARA', 'PT. JEMBATAN NUSANTARA', 1001, '-', ''),
-(75, 'KMP. MAHAKAM RAYA', 'PT. JEMBATAN NUSANTARA', 600, '-', ''),
-(76, 'KMP. TITIAN MURNI', 'PT. JEMBATAN NUSANTARA', 3614, '-', ''),
-(77, 'KMP. MABUAY NUSANTARA', 'PT. JEMBATAN NUSANTARA', 5035, '-', ''),
-(78, 'KMP. JEMBATAN MUSI II', 'PT. JEMBAATAN NUSANTARA', 159, '-', ''),
-(79, 'KMP. PERSADA NUSNTARA', 'PT. JEMBATAN NUSANTARA', 640, '-', ''),
-(80, 'KMP. MITRA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 5813, '-', ''),
-(81, 'KMP. MARINA SEGUNDA', 'PT. JEMBATAN NUSANTARA', 862, '-', ''),
-(82, 'KMP. MARINA PRIMERA', 'PT. JEMBATAN NUSANTARA', 824, '-', ''),
-(83, 'KMP. SWARNA NALINI', 'PT. JEMBATAN NUSANTARA', 323, '-', ''),
-(84, 'KMP. SRIKANDI NUSANTARA', 'PT. JEMBATAN NUSANTARA', 476, '-', ''),
-(85, 'KMP. SWARNA CAKRA', 'PT. JEMBATAN NUSANTARA', 799, '-', ''),
-(87, 'KMP. ROYAL NUSANTARA', 'PT. JEMBATAN NUSANTARA', 6034, '-', ''),
-(88, 'KMP. ANDIKA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 1229, '-', ''),
-(89, 'KMP. MARINA QUINTA', 'PT. JEMBATAN NUSANTARA', 821, '-', ''),
-(90, 'KMP. LASKAR PELANGI', 'PT. JEMBATAN NUSANTARA', 1001, '-', ''),
-(91, 'KMP. SAFIRA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 6345, '-', ''),
-(92, 'KMP. PERMATA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 1373, '-', ''),
-(93, 'KMP. JEMBATAN MUSI I', 'PT. JEMBATAN NUSANTARA', 406, '-', ''),
-(94, 'KMP. MANDALA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 1333, '-', ''),
-(95, 'KMP. SATRIA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 656, '-', ''),
-(96, 'KMP. RENNY II', 'PT. JEMBATAN NUSANTARA', 456, '-', ''),
-(97, 'KMP. SWARNA PUTRI', 'PT. JEMBATAN NUSANTARA', 516, '-', ''),
-(98, 'KMP. CITRA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 1007, '-', '');
+(1, 'KMP. PANORAMA NUSANTARA', 'PT. JEMBATAN NUSANTARA', 7965, '-', '');
 
 -- --------------------------------------------------------
 
@@ -355,115 +333,6 @@ CREATE TABLE `tbl_tanki` (
   `liter` float NOT NULL,
   `tinggi_maksimum` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_tanki`
---
-
-INSERT INTO `tbl_tanki` (`id`, `id_kapal`, `id_jenis_tanki`, `tinggi`, `liter`, `tinggi_maksimum`) VALUES
-(1, 1, 1, 159, 3500, 159),
-(2, 1, 2, 137, 3000, 137),
-(3, 1, 3, 183, 43870, 183),
-(4, 1, 4, 183, 43870, 183),
-(5, 74, 1, 80, 1200, 80),
-(6, 74, 2, 80, 1200, 80),
-(7, 74, 3, 80, 20000, 80),
-(8, 74, 4, 80, 20000, 80),
-(9, 75, 3, 1, 52, 144),
-(10, 75, 4, 1, 52, 144),
-(11, 75, 1, 83.3, 250, 83.3),
-(12, 75, 2, 83.3, 250, 83.3),
-(13, 76, 4, 88, 30800, 88),
-(14, 76, 7, 99, 1920, 99),
-(15, 76, 6, 102, 1860, 102),
-(16, 76, 8, 181, 1920, 181),
-(17, 77, 4, 97.6, 35200, 97.6),
-(18, 77, 6, 102.19, 1860, 102.19),
-(19, 77, 7, 101.58, 1920, 101.58),
-(20, 77, 8, 320, 1920, 320),
-(21, 78, 3, 205, 8000, 205),
-(22, 78, 4, 205, 8000, 205),
-(25, 78, 11, 120, 720, 120),
-(26, 79, 12, 183, 12770, 183),
-(27, 79, 1, 182, 700, 182),
-(28, 79, 2, 182, 700, 182),
-(29, 80, 13, 163, 17970, 163),
-(30, 80, 14, 189, 64090, 189),
-(31, 80, 15, 159, 64090, 159),
-(32, 80, 16, 135, 5500, 135),
-(33, 80, 17, 166, 2500, 166),
-(34, 81, 3, 324, 29000, 324),
-(35, 81, 4, 324, 29000, 324),
-(36, 81, 7, 290, 2900, 290),
-(37, 81, 6, 290, 2900, 290),
-(38, 81, 8, 69, 250, 69),
-(39, 82, 3, 220, 20000, 220),
-(40, 82, 4, 220, 20000, 220),
-(41, 82, 2, 122, 1500, 122),
-(42, 82, 1, 122, 1500, 122),
-(43, 83, 3, 278, 8000, 278),
-(44, 83, 4, 278, 8000, 278),
-(45, 83, 11, 130, 990, 130),
-(46, 83, 18, 67, 100, 67),
-(47, 84, 3, 380.76, 10850, 380.76),
-(48, 84, 4, 380.76, 10850, 380.76),
-(49, 84, 2, 55, 600, 55),
-(50, 85, 19, 108, 5900, 108),
-(51, 85, 16, 108, 5900, 108),
-(52, 85, 17, 133, 2400, 133),
-(53, 85, 18, 67, 100, 67),
-(54, 87, 20, 630, 172200, 630),
-(55, 87, 10, 301, 24100, 301),
-(56, 87, 8, 219, 3400, 219),
-(57, 87, 21, 49, 350, 49),
-(58, 87, 22, 95, 800, 95),
-(59, 88, 23, 353, 70500, 353),
-(60, 88, 10, 123, 1650, 123),
-(61, 88, 25, 73, 1650, 73),
-(62, 88, 24, 48, 240, 48),
-(63, 89, 26, 393, 29800, 393),
-(64, 89, 27, 393, 29800, 393),
-(65, 89, 28, 158, 1900, 158),
-(66, 89, 29, 158, 1900, 158),
-(67, 90, 34, 250, 5000, 250),
-(68, 90, 35, 250, 5000, 250),
-(69, 90, 36, 100, 500, 100),
-(70, 91, 37, 178, 28430, 178),
-(71, 91, 38, 238, 28430, 238),
-(72, 91, 16, 250, 11000, 250),
-(73, 91, 19, 184, 11000, 184),
-(74, 91, 39, 157, 5500, 157),
-(75, 91, 40, 155, 5500, 155),
-(76, 92, 23, 222, 81496, 222),
-(77, 92, 25, 130, 3120, 130),
-(78, 92, 8, 49, 112, 49),
-(79, 92, 9, 49, 112, 49),
-(80, 92, 21, 68, 158, 68),
-(81, 92, 41, 180, 3780, 180),
-(82, 93, 3, 114, 6500, 114),
-(83, 93, 1, 114, 6500, 114),
-(84, 93, 11, 83, 600, 83),
-(85, 93, 42, 25, 200, 25),
-(86, 93, 18, 31, 50, 31),
-(87, 93, 36, 38, 150, 38),
-(88, 94, 3, 147, 18600, 147),
-(89, 94, 4, 147, 18600, 147),
-(90, 94, 10, 100, 1500, 100),
-(91, 95, 4, 80, 6000, 80),
-(92, 95, 3, 80, 6000, 80),
-(93, 95, 10, 75, 900, 75),
-(94, 95, 8, 79, 750, 79),
-(95, 96, 2, 152, 1000, 152),
-(96, 96, 1, 152, 1000, 152),
-(97, 97, 23, 276, 25450, 276),
-(98, 97, 35, 90, 1200, 90),
-(99, 97, 34, 90, 1200, 90),
-(100, 98, 26, 149, 13260, 149),
-(101, 98, 27, 133, 13260, 133),
-(102, 98, 16, 104, 900, 104),
-(103, 98, 17, 103, 1051, 103),
-(104, 98, 43, 102, 1109, 102),
-(105, 85, 14, 261, 52100, 261);
 
 -- --------------------------------------------------------
 
@@ -612,7 +481,7 @@ ALTER TABLE `tbl_air_tawar`
 -- AUTO_INCREMENT for table `tbl_dermaga`
 --
 ALTER TABLE `tbl_dermaga`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_dokumen`
@@ -654,7 +523,7 @@ ALTER TABLE `tbl_kabid`
 -- AUTO_INCREMENT for table `tbl_kapal`
 --
 ALTER TABLE `tbl_kapal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_level`
@@ -666,13 +535,13 @@ ALTER TABLE `tbl_level`
 -- AUTO_INCREMENT for table `tbl_login`
 --
 ALTER TABLE `tbl_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_petugas`
 --
 ALTER TABLE `tbl_petugas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_sandar`
@@ -684,13 +553,13 @@ ALTER TABLE `tbl_sandar`
 -- AUTO_INCREMENT for table `tbl_tanki`
 --
 ALTER TABLE `tbl_tanki`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
